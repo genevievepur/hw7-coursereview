6609439af554871f33c7623b75375f3f591f90d8
Genevieve <upm6ce@virginia.edu>
2023-04-28 18:26:22 -0400

    created database and DatabaseManager class

---
+++ src/main/java/edu/virginia/cs/hw7/DatabaseManager.java
@@ -0,0 +1,12 @@
+package edu.virginia.cs.hw7;
+
+import javax.swing.plaf.nimbus.State;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseManager {
+    Connection connection;
+
+}
