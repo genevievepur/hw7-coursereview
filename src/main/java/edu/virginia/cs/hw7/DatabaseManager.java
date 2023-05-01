425c133f18b28a471a197cc2815ca4d85c1b8d0c
Genevieve <upm6ce@virginia.edu>
2023-04-30 21:23:24 -0400

    wrote basis of methods for adding into database tables

--- src/main/java/edu/virginia/cs/hw7/DatabaseManager.java
+++ src/main/java/edu/virginia/cs/hw7/DatabaseManager.java
@@ -1,10 +1,7 @@
 package edu.virginia.cs.hw7;
 
 import javax.swing.plaf.nimbus.State;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DatabaseManager {
     Connection connection;
@@ -52,5 +49,150 @@ public class DatabaseManager {
         } catch(SQLException e) {
             throw new IllegalStateException(e);
         }
+    }
+
+    public void addStudent(Student student) {
+        String name = student.getName();
+        String password = student.getPassword();
+
+        String insertQuery = String.format("""
+                INSERT INTO Students (ID, Name, Password)
+                VALUES (NULL, "%s", "%s");
+                """, name, password);
+
+        try {
+            Statement st = connection.createStatement();
+            st.execute(insertQuery);
+            st.close();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public boolean doesNameExist(String name) {
+        try {
+            Statement st = connection.createStatement();
+            String checkQuery = String.format("""
+                    SELECT * FROM Students WHERE Name = "%s";
+                    """, name);
+            ResultSet sameName = st.executeQuery(checkQuery);
+
+            if (sameName.next()) {
+                return true;
+            }
+
+            sameName.close();
+            st.close();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+        return false;
+    }
+
+    public void addCourse(Course course) {
+        String department = course.getDepartment();
+        int catalogNum = course.getCatalogNumber();
+
+        String insertQuery = String.format("""
+                INSERT INTO Courses (ID, Department, Catalog_Number)
+                VALUES (NULL, "%s", "%d");
+                """, department, catalogNum);
+
+        try {
+            Statement st = connection.createStatement();
+            st.execute(insertQuery);
+            st.close();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public boolean doesCourseExist(String department, int catalogNum) {
+        try {
+            Statement st = connection.createStatement();
+            String checkQuery = String.format("""
+                    SELECT * FROM Students WHERE Department = "%s" AND Catalog_Number = "%d";
+                    """, department, catalogNum);
+            ResultSet sameCourse = st.executeQuery(checkQuery);
+
+            if (sameCourse.next()) {
+                return true;
+            }
+
+            sameCourse.close();
+            st.close();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+        return false;
+    }
+
+    public void addReview(Review review) {
+        Student student = review.getStudent();
+        int studentID = getStudentID(student);
+
+        Course course = review.getCourse();
+        int courseID = getCourseID(course);
+
+        String insertQuery = String.format("""
+                INSERT INTO Reviews (ID, StudentID, CourseID, Text, Rating)
+                VALUES (NULL, "%d", "%d", "%s", "%d");
+                """, studentID, courseID, review.getText(), review.getRating());
+
+        try {
+            Statement st = connection.createStatement();
+            st.execute(insertQuery);
+            st.close();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public int getStudentID(Student student) {
+        String name = student.getName();
+        // String password = student.getPassword();
+        int studentID;
+
+        try {
+            Statement st = connection.createStatement();
+            String selectQuery = String.format("""
+                    SELECT * FROM Students WHERE Name = "%s";
+                    """, name);
+            ResultSet rs = st.executeQuery(selectQuery);
+
+            if (!rs.next()) {
+                throw new IllegalArgumentException("No such student exists in database.");
+            }
+
+            studentID = rs.getInt("ID");
+
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+        return studentID;
+    }
+
+    public int getCourseID(Course course) {
+        String department = course.getDepartment();
+        int catalogNum = course.getCatalogNumber();
+        int courseID;
+
+        try {
+            Statement st = connection.createStatement();
+            String selectQuery = String.format("""
+                    SELECT * FROM Courses WHERE Department = "%s" AND Catalog_Number = "%d";
+                    """, department, catalogNum);
+            ResultSet rs = st.executeQuery(selectQuery);
+
+            if (!rs.next()) {
+                throw new IllegalArgumentException("No such course exists in database.");
+            }
+
+            courseID = rs.getInt("ID");
+
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+        return courseID;
     }
 }
