88211099be149b60ee58d42e20a93b532e482c69
Genevieve <upm6ce@virginia.edu>
2023-05-04 00:47:52 -0400

    added javafx to project and created application and controller

---
+++ src/main/java/edu/virginia/cs/hw7/presentation/ReviewSystemApplication.java
@@ -0,0 +1,16 @@
+package edu.virginia.cs.hw7.presentation;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ReviewSystemApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
