f206346ab29b409034fd11d65c82e073537ccd59
tkamp <tlk6mp@virginia.edu>
2023-05-04 12:22:55 -0400

    created CourseReviewApp for javafx GUI extra credit portion

---
+++ src/main/java/edu/virginia/cs/hw7/presentation/CourseReviewApp.java
@@ -0,0 +1,20 @@
+package edu.virginia.cs.hw7.presentation;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class CourseReviewApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Your GUI code here
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
